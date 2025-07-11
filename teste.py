109b24452162bc145891330a85dd8d31663a8e92
Luis-Henrique523 <sales.luis@academico.ifpb.edu.br>
2025-07-11 13:14:31 +0000

    alteração

--- teste.py
+++ teste.py
@@ -1,14 +1,1 @@
-class Sexualidade():
-    
-    sexo = 'Masculino'
-
-    def __init__(self, nome):
-        self.nome = nome
-
-    @classmethod
-    def mudar_sexo(cls, nova_identificacao):
-        cls.sexo = nova_identificacao
-        return nova_identificacao
-    
-a = Sexualidade('Victoria')
-print(a.mudar_sexo('hetero'))
+print('ola mundo')
